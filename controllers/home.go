d06a12101c15a620c116583efc1b3c7891e68f02
arvinkai <ywy_sky@foxmail.com>
2016-05-10 18:03:44 +0800

    Edit template

--- controllers/home.go
+++ controllers/home.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"FD_WebServer/models"
+	"fmt"
+
 	"github.com/astaxie/beego"
 )
 
@@ -9,5 +12,10 @@ type HomePageController struct {
 }
 
 func (this *HomePageController) Get() {
+	pictures := models.GetPicture("Home")
+	this.Data["Pictures"] = pictures
+	this.Data["Picture0"] = pictures[0]
+	this.Data["Picture2"] = pictures[1]
+	fmt.Println(pictures[0])
 	this.TplName = "tab-subpage-home.html"
 }
--- models/fdweb_fun.go
+++ models/fdweb_fun.go
@@ -3,13 +3,32 @@ package models
 
 import (
 	"fmt"
+
+	"github.com/astaxie/beego/orm"
 )
 
-func GetPicture(Type string) (respones string) {
-	fmt.Println()
+func GetPicture(Type string) (resp []*Picture) {
+	fmt.Println(Type)
+	var ptc []*Picture = make([]*Picture, 0)
 	switch Type {
 	case "Home":
 		{
+			o := orm.NewOrm()
+			qs := o.QueryTable("picture")
+			_, err := qs.Filter("type", 1).Filter("isshow", 1).All(&ptc)
+			if err != nil {
+				fmt.Println(err)
+				return nil
+			}
+		}
+	case "shop":
+		{
+
 		}
 	}
+	return ptc
+}
+
+func GetGoodsInfo(Type string) string {
+	return "test"
 }
